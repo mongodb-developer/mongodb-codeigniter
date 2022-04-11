2e1e93a2f04ff079d786eecc32c5cc3eeca069ca
Stanimira Vlaeva <stanimira.vlaeva@gmail.com>
2022-04-11 18:46:34 +0300

    Add header and footer templates with Bootstrap 5 setup

---
+++ app/Views/templates/footer.php
@@ -0,0 +1,10 @@
+    </div>
+
+    <footer class="text-center">
+        <small>&copy; 2022</small>
+    </footer>
+
+    <!-- Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ app/Views/templates/header.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <!-- Required meta tags -->
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <!-- Load Bootstrap CSS -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+        <title>Reading Journal: MongoDB & CodeIgniter CRUD Tutorial</title>
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="/">Reading Journal</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="/books">Books List</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="/books/create">Add a Book</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+        <div class="container mt-5">
